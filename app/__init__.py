a9502b24c885176116a005967ca1ea47857c463b
catarium <60063501+catarium@users.noreply.github.com>
2020-08-22 12:18:56 +0300

    Bug fix

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,8 @@ app.config['RECAPTCHA_PRIVATE_KEY'] = '6LdTCbQZAAAAAHx_AZL_TND4HDGMPzdtn5_vNPTj'
 app.config['SECRET_KEY'] = 'AkalasH'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:MY_parol@localhost:5432/posts'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:MY_parol@localhost:5432/posts'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ttpkdgjdnewrkb:a786345bbea0adbfbe50e4b618a3350ecd1ac8028bd00e34d560e471a7bf5ccb@ec2-54-217-213-79.eu-west-1.compute.amazonaws.com:5432/ddq86s0s5prr5k'
 db = SQLAlchemy(app)
 
 
